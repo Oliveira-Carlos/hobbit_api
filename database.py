12d06376840307aaec7c092fad575e33560863ab
Oliveira-Carlos <carloseduardoipu01@gmail.com>
2024-12-12 22:55:46 -0300

    fix: Load credentials more securely into the database

--- database.py
+++ database.py
@@ -1,14 +1,23 @@
 from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-#Credenciais
+# Garantir que a URL do banco de dados seja carregada corretamente
 DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("A variável de ambiente DATABASE_URL não foi definida!")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)  # Adicionando pool_pre_ping para evitar falhas de conexão
 metadata = MetaData()
-Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Gerenciador de contexto para trabalhar com sessões
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
